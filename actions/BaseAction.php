3e1f77e957809afbab9fc09a1067e6e34c223782
萨耶摩尔丶槿花 <wzj-dev@AvalonSpacedeMacBook-Pro.local>
2018-02-27 15:14:19 +0800

    提交代码

---
+++ actions/BaseAction.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wzj-dev
+ * Date: 18/2/27
+ * Time: 下午2:57
+ */
+
+namespace sp_framework\actions;
+use yii\base\Action;
+
+class BaseAction extends Action
+{
+
+}
